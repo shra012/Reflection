ce79b3b9f60d14d27eb4ecdb84b94f82b16bea2b
shra012 <shravan_fisher@yahoo.ca>
2019-09-17 15:41:14 +0530

    HardCoded the version file

--- ci/build.sh
+++ ci/build.sh
@@ -2,7 +2,7 @@
 
 mkdir -p .m2/repository
 
-mvn -Dmaven.repo.local=.m2/repository
+mvn versions:set -DnewVersion=1.0-SNAPSHOT -Dmaven.repo.local=.m2/repository
 
 mvn package -Dmaven.repo.local=.m2/repository
 
