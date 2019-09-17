3a0386911f952c3b1ac98d73883a39d841f434a7
shra012 <shravan_fisher@yahoo.ca>
2019-09-17 15:38:47 +0530

    Removed maven version command.

--- ci/build.sh
+++ ci/build.sh
@@ -2,7 +2,7 @@
 
 mkdir -p .m2/repository
 
-mvn versions:set -DnewVersion=$(git rev-parse HEAD) -Dmaven.repo.local=.m2/repository
+mvn -Dmaven.repo.local=.m2/repository
 
 mvn package -Dmaven.repo.local=.m2/repository
 
